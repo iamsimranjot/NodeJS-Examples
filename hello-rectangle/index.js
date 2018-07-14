c541cfde5e2ca1e23dde69b8745ead4c4e605163
Simranjot Singh <iamsimranjot@gmail.com>
2018-07-14 13:55:36 +0530

    HTTP Yayy!

--- hello-rectangle/index.js
+++ hello-rectangle/index.js
@@ -1,8 +1,8 @@
 // Type I
 
 var rect = {
-	perimeter: (x,y) => (2(x+y)),
-	area: (x,y) => (x*y)
+  perimeter: (x, y) => (2(x + y)),
+  area: (x, y) => (x * y)
 };
 
 // Type II
@@ -11,34 +11,32 @@ const rect_exported = require('./rectangle.js')
 
 // Type III
 
-var perimeter = (x,y) =>  (2*(x+y));
-var area = (x,y) =>  (x*y);
+var perimeter = (x, y) => (2 * (x + y));
+var area = (x, y) => (x * y);
 
 
 // ----------------------------------------------------------------------------------------------------------------------------
 
-function solveRect(l,b) {
+function solveRect(l, b) {
 
-	console.log("Solving for rectangle with l = " + l + " and b = " + b);
-	rect_exported(l, b, (error, rectangle) => {
+  console.log("Solving for rectangle with l = " + l + " and b = " + b);
+  rect_exported(l, b, (error, rectangle) => {
 
-		if (error) {
+    if (error) {
 
-			console.log("Error: " + error.message)
-		}
-		else {
+      console.log("Error: " + error.message)
+    } else {
 
-			console.log("Perimeter for Length " + l + " and Breath " + b + " is " + rectangle.perimeter())
-			console.log("Area for Length " + l + " and Breath " + b + " is " + rectangle.area())
-		}
-	});	
+      console.log("Perimeter for Length " + l + " and Breath " + b + " is " + rectangle.perimeter())
+      console.log("Area for Length " + l + " and Breath " + b + " is " + rectangle.area())
+    }
+  });
 }
 
 
 // ----------------------------------------------------------------------------------------------------------------------------
 
-solveRect(2,4);
-solveRect(3,5);
-solveRect(0,5);
-solveRect(-3,5);
-
+solveRect(2, 4);
+solveRect(3, 5);
+solveRect(0, 5);
+solveRect(-3, 5);
--- hello-rectangle/rectangle.js
+++ hello-rectangle/rectangle.js
@@ -4,15 +4,15 @@ module.exports = (x, y, completion) => {
 
 		setTimeout(() => //  The reason why I'm using the setTimeout is to simulate a delay before the callback comes in from the other side.
 
-			completion(new Error("Rectangle dimensions should be greater than zero:  l = " + x + ",  and b = " + y), 
+			completion(new Error("Rectangle dimensions should be greater than zero:  l = " + x + ",  and b = " + y),
 			null),
 		 	2000)
 	}
 	else {
 
-		setTimeout(() => 
+		setTimeout(() =>
 
-			completion(null, 
+			completion(null,
 			{
 				perimeter: () => (2*(x+y)),
 				area: () => (x*y)
